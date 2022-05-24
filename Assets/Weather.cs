bafd78d8024b50c0e37c0a6d033dc66ceffc9af8
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-05-24 20:43:37 +0900

    22-05-24 Modify Weather Script Commit

--- Assets/Weather.cs
+++ Assets/Weather.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class Weather : MonoBehaviour
 {
+    public string[] weathers = { "맑음", "흐림", "비", "벼락"}; // 1 2 3 4 X 0 1 2 3
+    //public string[] menu_arr = new string[4];
+    public int x;
+
+    void PrintWeather()
+    {        
+        Debug.Log(GetWeather());
+    }
+
+    string GetWeather()
+    {        
+        return weathers[x];
+    }
+
+    void Update()
+    {
+        PrintWeather();
+    }
+
+
     void Start()
     {
         // List => 나중에 할것
         // Array => 배열
         //   변수를 모아둔 서랍
 
-        string[] menus = { "된장찌개", "김치찌개", "제육볶음", "두루치기" }; // 1 2 3 4 X 0 1 2 3
-        string[] menu_arr = new string[4];
-        menu_arr[0] = "짜장면";
+        /*menu_arr[0] = "짜장면";
         menu_arr[1] = "짬뽕";
         menu_arr[2] = "볶음밥";
-        menu_arr[3] = "울면";
-
 
         Debug.Log(menus[1]);
-        Debug.Log(menu_arr[2]);
-    }
-    void Update()
-    {
-        
+        Debug.Log(menu_arr[2]);*/
     }
 }
